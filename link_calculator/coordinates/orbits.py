82a9c9817bc88533a519324c4a8658e2d118d091
NinaA <averill.nina@gmail.com>
2022-06-19 22:46:25 +1000

    Added orbit equations

--- link_calculator/coordinates/orbits.py
+++ link_calculator/coordinates/orbits.py
@@ -391,6 +391,11 @@ class EllipticalOrbit(Orbit):
                 )
         return self._velocity
 
+    @property
+    def time_since_periapsis(self) -> float:
+        # TODO
+        pass
+
 
 class CircularOrbit(EllipticalOrbit):
     def __init__(
@@ -572,9 +577,11 @@ class HyperbolicOrbit(Orbit):
         velocity: float = None,
         escape_velocity: float = None,
         angle_of_asymptote: float = None,
+        asymptote_true_anomaly: float = None,
         mu: float = EARTH_MU,
     ):
         self._angle_of_asymptote = self._angle_of_asymptote
+        self._asymptote_true_anomaly = asymptote_true_anomaly
         super().__init__(
             semimajor_axis=semimajor_axis,
             semiminor_axis=semiminor_axis,
@@ -614,3 +621,188 @@ class HyperbolicOrbit(Orbit):
                     / (self.semiminor_axis**2 * self.periapsis_radius**2)
                 )
         return self._angle_of_asymptote
+
+    @property
+    def eccentricity(self) -> float:
+        """
+        Equations from Spacecraft Mission Design (1998)
+        """
+        if self._eccentricity is None:
+            if self._isset(self._angle_of_asymptote):
+                # (2.66)
+                self._eccentricity = 1 / cos(radians(self.angle_of_asymptote))
+            elif self._isset(self._semimajor_axis, self._periapsis_radius):
+                # (2.67)
+                self._eccentricity = 1 + self.periapsis_radius / self.semimajor_axis
+            elif self._isset(self._semimajor_axis, self._semiminor_axis):
+                # (2.68)
+                self._eccentricity = sqrt(
+                    1 + self.semiminor_axis**2 / self.semimajor_axis**2
+                )
+        return self._eccentricity
+
+    @property
+    def flight_path_angle(self):
+        """
+        Equations from Spacecraft Mission Design (1998)
+        """
+        if self._flight_path_angle is None:
+            if self._isset(self._eccentriciy, self._true_anomaly):
+                # (2.31)
+                self._flight_path_angle = atan2(
+                    1 + self.eccentricity * cos(radians(self.true_anomaly)),
+                    self.eccentricity * sin(radians(self.true_anomaly)),
+                )
+        return self._flight_path_angle
+
+    @property
+    def mean_motion(self) -> float:
+        """
+        Equations from Spacecraft Mission Design (1998)
+        """
+        if self._mean_motion is None:
+            if self._isset(self._semimajor_axis):
+                # (2.36)
+                self._mean_motion = sqrt(self.mu / self.semimajor_axis**3)
+        return self._mean_motion
+
+    @property
+    def orbital_radius(self) -> float:
+        """
+        Equations from Spacecraft Mission Design (1998)
+        """
+        if self._orbital_radius is None:
+            if self._isset(
+                self._semimajor_axis, self._eccentricity, self._true_anomaly
+            ):
+                # (2.51)
+                self._orbital_radius = (
+                    self.semimajor_axis
+                    * (self.eccentricity**2 - 1)
+                    / (1 + self.eccentricity * cos(radians(self.true_anomaly)))
+                )
+        return self._orbital_radius
+
+    @property
+    def periapsis_radius(self) -> float:
+        """
+        Equations from Spacecraft Mission Design (1998)
+        """
+        if self._periapsis_radius is None:
+            if self._isset(self._semiminor_axis, self._eccentricity):
+                # (2.69)
+                self._periapsis_radius = self.semiminor_axis * sqrt(
+                    (self.eccentricity - 1) / (self.eccentricity + 1)
+                )
+            elif self._isset(self._semimajor_axis, self._eccentricity):
+                # (2.70)
+                self._periapsis_radius = self.semimajor_axis * (self.eccentricity - 1)
+            elif self._isset(self._semiminor_axis, self._angle_of_asymptote):
+                # (2.72)
+                self._periapsis_radius = self.semiminor_axis * tan(
+                    radians(self.angle_of_asymptote / 2)
+                )
+            elif self._isset(self._semimajor_axis, self._semiminor_axis):
+                # (2.75)
+                self._periapsis_radius = -self.semi_major_axis + sqrt(
+                    self.semimajor_axis**2 + self.semiminor_axis**2
+                )
+            # TODO (2.73)
+            # TODO (2.44)
+        return self._periapsis_radius
+
+    @property
+    def semimajor_axis(self) -> float:
+        """
+        Equations from Spacecraft Mission Design (1998)
+        """
+        if self._semimajor_axis is None:
+            if self._isset(self._eccentricity, self._semiminor_axis):
+                # (2.76)
+                self._semimajor_axis = self.semiminor_axis / sqrt(
+                    self.eccentricity**2 - 1
+                )
+            elif self._isset(self._periapsis_radius, self._eccentricity):
+                # (2.77)
+                self._semimajor_axis = self.periapsis_radius / (self.eccentricity - 1)
+            elif self._isset(self._semiminor_axis, self._periapsis_radius):
+                # (2.79)
+                self._semimajor_axis = (
+                    self.semiminor_axis**2 - self.periapsis_radius**2
+                ) / (2 * self.periapsis_radius)
+            # TODO (2.78)
+            # TODO (2.80)
+        return self._semimajor_axis
+
+    @property
+    def semiminor_axis(self) -> float:
+        """
+        Equations from Spacecraft Mission Design (1998)
+        """
+        if self._semiminor_axis is None:
+            if self._isset(self._periapsis_radius, self._eccentricity):
+                # (2.81)
+                self._semiminor_axis = self.periapsis_radius * sqrt(
+                    (self.eccentricity + 1) / (self.eccentricity - 1)
+                )
+            elif self._isset(self._semimajor_axis, self._eccentricity):
+                # (2.82)
+                self._semiminor_axis = self.semimajor_axis * sqrt(
+                    self.eccentricity**2 - 1
+                )
+        # TODO (2.83)
+        return self._semiminor_axis
+
+    @property
+    def time_since_periapsis(self) -> float:
+        """
+        Equations from Spacecraft Mission Design (1998)
+        """
+        # TODO
+        pass
+
+    @property
+    def true_anomaly(self) -> float:
+        """
+        Equations from Spacecraft Mission Design (1998)
+        """
+        if self._true_anomaly is None:
+            if self._isset(
+                self._semimajor_axis, self._eccentricity, self._orbital_radius
+            ):
+                # (2.52)
+                self._true_anomaly = (
+                    self.semimajor_axis
+                    * (self.eccentricity**2 - 1)
+                    / (self.orbital_radius * self.eccentricity)
+                    - 1 / self.eccenricity
+                )
+        return self._true_anomaly
+
+    @property
+    def asymptote_true_anomaly(self) -> float:
+        """
+        Equations from Spacecraft Mission Design (1998)
+        """
+        if self._asymptote_true_anomaly is None:
+            if self._isset(self._eccentricity):
+                # (2.54)
+                self._asymptote_true_anomaly = acos(-1 / self.eccentricity)
+            # TODO (2.53)
+        return self._asymptote_true_anomaly
+
+    @property
+    def velocity(self) -> float:
+        """
+        Equations from Spacecraft Mission Design (1998)
+        """
+        if self._velocity is None:
+            if self._isset(self._orbital_radius, self._semimajor_axis):
+                # (2.16)
+                self._velocity = sqrt(
+                    (2 * self.mu) / self.orbital_radius + self.mu / self.semimajor_axis
+                )
+            # TODO (2.56)
+            # TODO (2.57)
+            # TODO (2.58)
+        return self._velocity
